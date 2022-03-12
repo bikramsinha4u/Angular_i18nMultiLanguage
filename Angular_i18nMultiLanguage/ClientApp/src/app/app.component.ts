7660ed3d375b0c802aaa3134c32fcbbcb39d1115
Bikram Sinha <bikram.sinha@siemens-healthineers.com>
2022-03-12 13:53:02 +0530

    locale ui and locale settings

--- Angular_i18nMultiLanguage/ClientApp/src/app/app.component.ts
+++ Angular_i18nMultiLanguage/ClientApp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject, LOCALE_ID } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ClientApp';
+
+  languageList = [
+    { code: 'en', label: 'English' },
+    { code: 'fr', label: 'French' }
+  ];
+  constructor(@Inject(LOCALE_ID) protected localeId: string) { }
 }
